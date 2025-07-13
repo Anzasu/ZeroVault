00010c70f2efd5fc282b572b6dc3de70a2c1238a
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2025-07-13 10:28:11 +0000

    Added primitive navigation to lock screen

--- lib/pages/lock_screen/lock_screen_model.dart
+++ lib/pages/lock_screen/lock_screen_model.dart
@@ -3,6 +3,7 @@ import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
 import '/flutter_flow/flutter_flow_widgets.dart';
 import 'dart:ui';
+import '/index.dart';
 import 'lock_screen_widget.dart' show LockScreenWidget;
 import 'package:pin_code_fields/pin_code_fields.dart';
 import 'package:flutter/material.dart';
--- lib/pages/lock_screen/lock_screen_widget.dart
+++ lib/pages/lock_screen/lock_screen_widget.dart
@@ -3,6 +3,7 @@ import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
 import '/flutter_flow/flutter_flow_widgets.dart';
 import 'dart:ui';
+import '/index.dart';
 import 'package:pin_code_fields/pin_code_fields.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -200,8 +201,8 @@ class _LockScreenWidgetState extends State<LockScreenWidget> {
                             color: Color(0xFF0077FF),
                             size: 50.0,
                           ),
-                          onPressed: () {
-                            print('IconButton pressed ...');
+                          onPressed: () async {
+                            context.pushNamed(VaultWidget.routeName);
                           },
                         ),
                       ),
